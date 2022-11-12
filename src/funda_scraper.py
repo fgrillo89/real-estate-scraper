cd634cf51e530d437c9ae5956c74d709925b439c
fgrillo <fabio.grillo@mat.ethz.ch>
2022-11-12 20:43:08 +0100

    refactored FundaScraper / scrape_shallow now returns a DataFrame

--- src/funda_scraper.py
+++ src/funda_scraper.py
@@ -1,10 +1,15 @@
 import json
 from pathlib import Path
+from typing import Union
+from dataclasses import dataclass
 
+import pandas as pd
 from pipe import traverse, select, sort, where
 import asyncio
 import re
+from enum import Enum
 from city_scraper import Scraper
+from parsing import str_from_tag
 
 config_path = Path.cwd() / 'config' / 'config.json'
 
@@ -13,6 +18,40 @@ with open(config_path) as json_file:
 
 HEADER = api_config['headers']
 URL = "https://www.funda.nl/en/koop/{}/p{}"
+
+
+class HouseDetailsShallow(Enum):
+    address = 'Address'
+    postcode = 'PostCode'
+    living_area = 'LivingArea'
+    plot_size = 'PlotSize'
+    price = 'Price'
+    rooms = 'Rooms'
+
+
+def extract_number_of_rooms(soup):
+    result = soup.find('ul', class_="search-result-kenmerken").find_all('li')
+    if len(result) > 1:
+        return result[1]
+
+
+detail_method_map_sh = {HouseDetailsShallow.address: lambda soup: soup.find('h2'),
+                        HouseDetailsShallow.postcode: lambda soup: soup.find('h4'),
+                        HouseDetailsShallow.living_area: lambda soup: soup.find(attrs={'title': 'Living area'}),
+                        HouseDetailsShallow.plot_size: lambda soup: soup.find(attrs={'title': 'Plot size'}),
+                        HouseDetailsShallow.price: lambda soup: soup.find('span', class_='search-result-price'),
+                        HouseDetailsShallow.rooms: extract_number_of_rooms
+                        }
+
+
+def parse_shallow_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    for key in [HouseDetailsShallow.price,
+                HouseDetailsShallow.plot_size,
+                HouseDetailsShallow.living_area,
+                HouseDetailsShallow.rooms]:
+        df[key.value] = pd.to_numeric(df[key.value].str.replace('\D+', '', regex=True))
+
+    return df
 
 
 class FundaScraper(Scraper):
@@ -34,19 +73,23 @@ class FundaScraper(Scraper):
         num_listings = json.loads(soup.find_all('script', type="application/ld+json")[2].text)['results_total']
         return num_pages, num_listings
 
-    async def scrape_shallow(self, city='heel-nederland', max_pp=None) -> list:
-        listings = []
-        if max_pp is None:
-            max_pp, _ = await self._get_number_of_pages_and_listings(city)
+    def scrape_shallow(self, city='heel-nederland', pages: Union[None, list[int]] = None) -> list:
+        return asyncio.run(self.scrape_shallow_async(city, pages))
 
-        soups = await asyncio.gather(*(self._get_soup(city, i) for i in range(1, max_pp + 1)))
+    async def scrape_shallow_async(self, city='heel-nederland', pages: Union[None, list[int]] = None) -> pd.DataFrame:
+        house_data = []
+        if pages is None:
+            max_pp, _ = await self._get_number_of_pages_and_listings(city)
+            pages = range(1, max_pp + 1)
+
+        soups = await asyncio.gather(*(self._get_soup(city, i) for i in pages))
 
         for soup in soups:
             results = self.get_main_page_results(soup)
-            details = [self.get_house_details(result) for result in results]
-            listings = listings + details
+            houses = [self.get_house_details(result) for result in results]
+            house_data = house_data + houses
 
-        return listings
+        return parse_shallow_dataframe(pd.DataFrame(house_data))
 
     @staticmethod
     def get_main_page_results(soup):
@@ -54,24 +97,12 @@ class FundaScraper(Scraper):
 
     @staticmethod
     def get_house_details(result):
-        details = {}
+        house = {}
 
-        extract_number = lambda x: int(re.search('\d+', x.get_text(strip=True)).group()) if x else None
-        extract_text = lambda x: x.get_text(strip=True) if x else None
+        for detail in HouseDetailsShallow:
+            house[detail.value] = str_from_tag(detail_method_map_sh[detail](result))
 
-        details['Address'] = extract_text(result.find('h2'))
-        details['Postcode'] = extract_text(result.find('h4'))
-
-        details['PlotSize'] = extract_number(result.find(attrs={'title': 'Plot size'}))
-        details['LivingArea'] = extract_number(result.find(attrs={'title': 'Living area'}))
-        details['Price'] = ''.join(
-            re.findall('\d+', result.find('span', class_='search-result-price').get_text(strip=True)))
-        # details['Rooms'] = extract_number(result.find('ul', class_="search-result-kenmerken").find_all('li')[1])
-
-        details['Agency'] = extract_text(result.find('a', class_='search-result-makelaar'))
-        if details['Address'] is None:
-            print("Address not found")
-        return details
+        return house
 
     # async def get_all_children_urls(self, ):
     #     max_pp = await get_num_pp_from_main_page(url.format(1))
@@ -83,3 +114,4 @@ class FundaScraper(Scraper):
 
 if __name__ == '__main__':
     scraper = FundaScraper()
+    results = scraper.scrape_shallow(city='Delft', pages=None)
--- src/parsing.py
+++ src/parsing.py
@@ -19,24 +19,24 @@ def str_from_tag(tag: Tag, strip=True, **kwargs) -> Union[None, str]:
     except AttributeError as e:
         print(e)
 
-
-def digits_from_str(string: str, join=False) -> Union[None, int, list[int]]:
-    """
-    Extracts digits from a string
-    :param string:
-    :param join:
-    :return:
-    """
-    if string is None:
-        return None
-
-    digits_list = re.findall('\d+', string)
-
-    if not digits_list:
-        return None
-
-    if join:
-        digits_str = ''.join(digits_list)
-        return int(digits_str)
-
-    return list(map(int, digits_list))
+#
+# def digits_from_str(string: str, join=False) -> Union[None, int, list[int]]:
+#     """
+#     Extracts digits from a string
+#     :param string:
+#     :param join:
+#     :return:
+#     """
+#     if string is None:
+#         return None
+#
+#     digits_list = re.findall('\d+', string)
+#
+#     if not digits_list:
+#         return None
+#
+#     if join:
+#         digits_str = ''.join(digits_list)
+#         return int(digits_str)
+#
+#     return list(map(int, digits_list))
