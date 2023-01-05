711a2a5ff91f6012748932923cbcdd5218ed2b80
fgrillo89 <fabio.grillo89@gmail.com>
2023-01-05 16:28:08 +0100

    Shortened docstrings

--- real_estate_scraper/html_handling.py
+++ real_estate_scraper/html_handling.py
@@ -4,30 +4,13 @@ import requests
 
 
 def get_html_synch(url_str: str, header: dict) -> str:
-    """Get the HTML content of a webpage synchronously.
-
-    Args:
-        url_str (str): The URL of the webpage.
-        header (dict): A dictionary containing the HTTP headers to use for the request.
-
-    Returns:
-        str: The HTML content of the webpage.
-    """
-
+    """Get the HTML content of a webpage synchronously."""
     response = requests.request("GET", url_str, headers=header)
     return response.text
 
 
 async def get_html(url: str, header: dict) -> str:
-    """Get the HTML content of a webpage asynchronously.
-
-    Args:
-        url (str): The URL of the webpage.
-        header (dict): A dictionary containing the HTTP headers to use for the request.
-
-    Returns:
-        str: The HTML content of the webpage.
-    """
+    """Get the HTML content of a webpage asynchronously."""
     return await asyncio.to_thread(get_html_synch, url, header)
 
 
