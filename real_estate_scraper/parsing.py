66eb4c3fdf0c14064fd97e1c1f479e3fb2983399
fgrillo89 <fabio.grillo89@gmail.com>
2023-01-05 18:16:41 +0100

    Reformatted docstring

--- real_estate_scraper/parsing.py
+++ real_estate_scraper/parsing.py
@@ -6,13 +6,7 @@ from real_estate_scraper.configuration import NamedItemsDict
 
 
 def str_from_tag(tag: Tag, strip=True, **kwargs) -> Union[None, str]:
-    """
-    Get the text of a BeautifulSoup tag
-    :param tag:
-    :param strip:
-    :param kwargs:
-    :return:
-    """
+    """Get the text of a BeautifulSoup tag"""
     if tag is None:
         return None
 
@@ -24,6 +18,7 @@ def str_from_tag(tag: Tag, strip=True, **kwargs) -> Union[None, str]:
 
 
 def parse_dataframe(house_attributes: NamedItemsDict, df: pd.DataFrame) -> pd.DataFrame:
+    """Convert the values in the dataframe to the appropriate data type."""
     for attribute in house_attributes:
         if attribute.type == 'numeric':
             df[attribute.name] = pd.to_numeric(df[attribute.name].str.replace('\D+', '', regex=True))
