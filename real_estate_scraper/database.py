af966f46831c1bb62a90fc5305baaa6a16f305a2
fgrillo <fabio.grillo@mat.ethz.ch>
2023-01-14 00:00:10 +0100

    Added a function to load data from a sqlite db to DataFrame

--- real_estate_scraper/database.py
+++ real_estate_scraper/database.py
@@ -1,60 +1,18 @@
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Column, Integer, String, create_engine
-#
-# Base = declarative_base()
-#
-#
-# class User(Base):
-#     __tablename__ = 'users'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String)
-#
-#
-# class Post(Base):
-#     __tablename__ = 'posts'
-#     id = Column(Integer, primary_key=True)
-#     title = Column(String)
-#     body = Column(String)
-#
-#
-# engine = create_engine('sqlite:///my_database.db')
-# engine.create_all([User, Post])
-#
-# from sqlalchemy import Table, Column, MetaData
-#
-#
-# def create_table(table_name, columns):
-#     # Create a MetaData object
-#     metadata = MetaData()
-#
-#     # Define the columns of the table
-#     columns = [
-#         Column(col_name, col_type)
-#         for col_name, col_type in columns.items()
-#     ]
-#
-#     # Create the table using the columns and metadata
-#     table = Table(table_name, metadata, *columns)
-#
-#     return table
-#
-#
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Column, Integer, String
-#
-#
-# def create_table(table_name, columns):
-#     # Create a declarative base class
-#     Base = declarative_base()
-#
-#     # Define a class that represents the table
-#     class Table(Base):
-#         __tablename__ = table_name
-#
-#         # Define the columns of the table
-#         columns = [
-#             Column(col_name, col_type)
-#             for col_name, col_type in columns.items()
-#         ]
-#
-#     return Table
+from pathlib import Path
+from typing import Optional
+
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+def load_data(db_path: str, table_name: str, columns: Optional[list] = None) -> pd.DataFrame:
+    """loads data from a sqlite database to a pandas dataframe"""
+    if not Path(db_path).is_file():
+        raise ValueError(f"Error: the {db_path} database does not exist.")
+    engine = create_engine(f'sqlite:///{db_path}')
+    if columns:
+        query = f"SELECT {', '.join(columns)} from {table_name}"
+        df = pd.read_sql(query, engine)
+    else:
+        df = pd.read_sql(table_name, engine)
+    return df
