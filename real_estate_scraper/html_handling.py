a0cffba4df309315254f08423f8190ea901b8bec
fgrillo <fabio.grillo@mat.ethz.ch>
2023-01-16 19:59:30 +0100

    renamed get_html% to get_response% -> Reduced the responsibility of the function to only return a response object, instead of text. This is to allow more flexible of the response downstream.

--- real_estate_scraper/html_handling.py
+++ real_estate_scraper/html_handling.py
@@ -3,15 +3,12 @@ import asyncio
 import requests
 
 
-def get_html_synch(url_str: str, header: dict) -> str:
-    """Get the HTML content of a webpage synchronously."""
-    response = requests.request("GET", url_str, headers=header)
-    return response.text
+def get_response_synch(url_str: str, header: dict) -> requests.Response:
+    return requests.request("GET", url_str, headers=header)
 
 
-async def get_html(url: str, header: dict) -> str:
-    """Get the HTML content of a webpage asynchronously."""
-    return await asyncio.to_thread(get_html_synch, url, header)
+async def get_response(url: str, header: dict) -> requests.Response:
+    return await asyncio.to_thread(get_response_synch, url, header)
 
 
 if __name__ == "__main__":
