c3108633710ee7487dace15437f236c29372d3cd
fgrillo <fabio.grillo@mat.ethz.ch>
2023-01-22 20:08:24 +0100

    Wrote and tested function for extracting the text before and within brackets in strings such as "semi-detached residential property (patio residence)"

--- real_estate_scraper/parsing.py
+++ real_estate_scraper/parsing.py
@@ -4,8 +4,6 @@ from typing import Union, Optional, Tuple
 import pandas as pd
 from bs4.element import Tag
 
-from real_estate_scraper.configuration import NamedHouseItems
-
 
 def str_from_tag(tag: Tag, strip=True, **kwargs) -> Union[None, str]:
     """Get the text of a BeautifulSoup tag"""
@@ -19,8 +17,9 @@ def str_from_tag(tag: Tag, strip=True, **kwargs) -> Union[None, str]:
         print(e)
 
 
-def extract_numeric_value(string: str, decimal_delimiter: str = ".",
-                          thousands_delimiter: str = ",") -> Optional[float]:
+def extract_numeric_value(
+        string: str, decimal_delimiter: str = ".", thousands_delimiter: str = ","
+) -> Optional[float]:
     """Extracts the numeric value from a string, takes into account the unit of
     measure, the thousands' delimiter, the decimal delimiter, and ignores any character
     or space before a digit"""
@@ -32,7 +31,7 @@ def extract_numeric_value(string: str, decimal_delimiter: str = ".",
         return None
     string = string.replace(thousands_delimiter, "")
     string = string.replace(decimal_delimiter, ".")
-    match = re.search(r'(?<![^\s])\d+(?:\.\d+)?(?![^\s])', string)
+    match = re.search(r"(?<![^\s])\d+(?:\.\d+)?(?![^\s])", string)
     if match:
         numeric_value = float(match.group())
     else:
@@ -41,7 +40,6 @@ def extract_numeric_value(string: str, decimal_delimiter: str = ".",
 
 
 def extract_rooms_and_bedrooms(string: str) -> Optional[Tuple[float, Optional[float]]]:
-
     rooms, bedrooms = None, None
 
     if not string:
@@ -56,8 +54,27 @@ def extract_rooms_and_bedrooms(string: str) -> Optional[Tuple[float, Optional[fl
     return rooms, bedrooms
 
 
-def get_retrieval_statistics(df: pd.DataFrame, items_list: list[str]) \
-        -> tuple[float, int, int]:
+def extract_text_before_and_within_brackets(string: str) \
+        -> Optional[Tuple[str, Optional[str]]]:
+
+    before_brackets, within_brackets = None, None
+
+    if not string:
+        return before_brackets, within_brackets
+
+    match = re.search(r"([^()]+)(?:\(([^)]*)\))?", string)
+
+    if match:
+        before_brackets = match.group(1).strip() if match.group(1).strip() != '' else None
+        within_brackets = match.group(2).strip() if match.group(2) else None
+
+    return before_brackets, within_brackets
+
+
+
+def get_retrieval_statistics(
+        df: pd.DataFrame, items_list: list[str]
+) -> tuple[float, int, int]:
     """Get the retrieval statistics for a list of items"""
 
     nan_count = df[items_list].isnull().sum(axis=1)
