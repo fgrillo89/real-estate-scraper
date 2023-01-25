1d94e74dbbfe6c929979cea131876a994c6bb32b
fgrillo <fabio.grillo@mat.ethz.ch>
2023-01-25 16:16:26 +0100

    Refactored get_response to use aiohttp instead of requests; now get_response reads the content either as text or by serializing a json depending on the input argument "read_format". Also, added timeout and handling of ClientError.

--- real_estate_scraper/html_handling.py
+++ real_estate_scraper/html_handling.py
@@ -1,15 +1,42 @@
-import asyncio
+from typing import Union
 
-import requests
-
-
-def get_response_synch(url_str: str, header: dict) -> requests.Response:
-    return requests.request("GET", url_str, headers=header)
+import aiohttp
 
 
-async def get_response(url: str, header: dict) -> requests.Response:
-    return await asyncio.to_thread(get_response_synch, url, header)
+async def get_response(url_str: str,
+                       header: dict,
+                       read_format: str = "text",
+                       max_retries: int = 3,
+                       timeout: int = 10) -> Union[str, dict, list]:
+    retries = 0
+    while retries < max_retries:
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url_str,
+                                       headers=header,
+                                       timeout=timeout) as response:
+                    response.raise_for_status()
+                    return await process_response(response, read_format=read_format)
+        except aiohttp.ClientError as e:
+            retries += 1
+            if retries == max_retries:
+                raise e
+            print(f'Retrying request to {url_str} (attempt {retries}/{max_retries})')
 
+
+async def process_response(response: aiohttp.ClientResponse, read_format: str = "text") \
+        -> Union[str, dict, list]:
+    method_factory = {"text": lambda x: x.content.read(),
+                      "json": lambda x: x.content.json()}
+    return await method_factory[read_format](response)
+
+
+# def get_response_synch(url_str: str, header: dict) -> requests.Response:
+#     return requests.request("GET", url_str, headers=header)
+
+
+# async def get_response(url: str, header: dict) -> requests.Response:
+#     return await asyncio.to_thread(get_response_synch, url, header)
 
 if __name__ == "__main__":
     pass
