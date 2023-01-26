0b107d4ff01cec762360d71c52e24540d0f1b057
fgrillo89 <fabio.grillo89@gmail.com>
2023-01-26 11:37:53 +0100

    Added decorators to add AsyncLimiter and Semaphore context managers to async functions. Added async get_soup and get_json functions

--- real_estate_scraper/html_handling.py
+++ real_estate_scraper/html_handling.py
@@ -1,8 +1,12 @@
 import asyncio
 import logging
+from asyncio import Semaphore
+from functools import wraps
 from typing import Union, Optional
 
 import aiohttp
+from aiolimiter import AsyncLimiter
+from bs4 import BeautifulSoup
 
 
 async def get_response(url_str: str,
@@ -24,7 +28,7 @@ async def get_response(url_str: str,
             retries += 1
             if retries == max_retries:
                 raise e
-            msg = f"Retrying request to {url_str} (attempt {retries}/{max_retries})"\
+            msg = f"Retrying request to {url_str} (attempt {retries}/{max_retries})" \
                   f"because of {e}"
             if logger:
                 logging.warning(msg)
@@ -46,6 +50,43 @@ async def process_response(response: aiohttp.ClientResponse, read_format: str = 
     return await method_factory[read_format](response)
 
 
+def add_limiter(limiter: AsyncLimiter):
+    def inner(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            async with limiter:
+                return await func(*args, **kwargs)
+
+        return wrapper
+
+    return inner
+
+
+def add_semaphore(semaphore: Semaphore):
+    def inner(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            async with semaphore:
+                return await func(*args, **kwargs)
+
+        return wrapper
+
+    return inner
+
+
+async def get_soup(url: str,
+                   header: Optional[dict[str]] = None,
+                   parse_only: Optional[list[str]] = None,
+                   logger: Optional[logging.Logger] = None) -> BeautifulSoup:
+    response = await get_response(url, header=header, logger=logger)
+    return BeautifulSoup(response, "lxml", parse_only=parse_only)
+
+
+async def get_json(url: str,
+                   header: Optional[dict[str]] = None,
+                   logger: Optional[logging.Logger] = None) -> BeautifulSoup:
+    return await get_response(url, header=header, logger=logger, read_format='json')
+
 # def get_response_synch(url_str: str, header: dict) -> requests.Response:
 #     return requests.request("GET", url_str, headers=header)
 
@@ -54,4 +95,19 @@ async def process_response(response: aiohttp.ClientResponse, read_format: str = 
 #     return await asyncio.to_thread(get_response_synch, url, header)
 
 if __name__ == "__main__":
-    pass
+    url = "https://www.funda.nl/en/koop/eede/huis-88953000-scheidingstraat-27/"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ("
+                      "KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+    }
+
+    limit = AsyncLimiter(1, round(1 / 5, 3))
+    sem = Semaphore(value=5)
+
+    @add_semaphore(sem)
+    @add_limiter(limit)
+    async def limited_get_soup(*args, **kwargs):
+        return get_soup(*args, **kwargs)
+
+
+    soup = asyncio.run(get_soup(url=url, header=headers))
