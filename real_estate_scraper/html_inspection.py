726193dc9a7a2da9c2145f2f170179102f4a5219
fgrillo89 <fabio.grillo89@gmail.com>
2023-01-26 17:15:36 +0100

    Added some helper functions

--- real_estate_scraper/html_inspection.py
+++ real_estate_scraper/html_inspection.py
@@ -1,11 +1,13 @@
 import asyncio
 from asyncio import Semaphore
-from typing import Union
+from typing import Union, Optional
 
 from aiolimiter import AsyncLimiter
 from bs4 import BeautifulSoup
+from pipe import traverse
 
 from real_estate_scraper.html_handling import get_soup, add_limiter, add_semaphore
+from real_estate_scraper.parsing import str_from_tag
 
 MAX_ACTIVE_REQUESTS = 5
 REQUESTS_PER_SECOND = 5
@@ -41,21 +43,53 @@ def get_dd_text_from_dt_name(soup, text_in_website):
         return None
 
 
+def extract_all_dt(soup: BeautifulSoup) -> Optional[list[dict[str, str]]]:
+    dt_list = soup.find_all("dt")
+    if dt_list:
+        return [str_from_tag(dt) for dt in dt_list]
+    return None
+
+
+def extract_all_dd_text(soup: BeautifulSoup, dt_names: list[str]):
+    items = {}
+    for dt_name in dt_names:
+        items[dt_name] = get_dd_text_from_dt_name(soup, dt_name)
+    return items
+
+
 def main():
-    url_template = "https://www.immobiliare.it/vendita-case/{}/?criterio=rilevanza&pag={}"
+    urls = ['https://www.immobiliare.it/annunci/98531352/',
+            'https://www.immobiliare.it/annunci/95014054/',
+            'https://www.immobiliare.it/annunci/99087248/',
+            'https://www.immobiliare.it/annunci/99709996/',
+            'https://www.immobiliare.it/annunci/92273868/',
+            'https://www.immobiliare.it/annunci/100508874/',
+            'https://www.immobiliare.it/annunci/99742246/',
+            'https://www.immobiliare.it/annunci/100035388/',
+            'https://www.immobiliare.it/annunci/98811448/',
+            'https://www.immobiliare.it/annunci/99706304/',
+            'https://www.immobiliare.it/annunci/99509624/',
+            'https://www.immobiliare.it/annunci/100160892/',
+            'https://www.immobiliare.it/annunci/93174362/',
+            'https://www.immobiliare.it/annunci/100603786/',
+            'https://www.immobiliare.it/annunci/94219346/',
+            'https://www.immobiliare.it/annunci/100027704/',
+            'https://www.immobiliare.it/annunci/99100210/',
+            'https://www.immobiliare.it/annunci/97474264/',
+            'https://www.immobiliare.it/annunci/94935172/',
+            'https://www.immobiliare.it/annunci/100632063/']
 
-    get_url = lambda city, page: url_template.format(city, page)
-
-    pages = [1, 2, 3]
-    urls = [get_url("roma", page) for page in pages]
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ("
                       "KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
     }
 
     soups = get_soups(urls, header=headers)
-    return soups
+
+    unique_items = set([extract_all_dt(soup) for soup in soups] | traverse)
+
+    return soups, unique_items
 
 
 if __name__ == "__main__":
-    results = main()
+    results, unique_items = main()
