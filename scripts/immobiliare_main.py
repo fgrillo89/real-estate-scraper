cc9ae5424fd85dd2812d65f216a4b794a6cb4783
fgrillo89 <fabio.grillo89@gmail.com>
2023-01-30 16:38:15 +0100

    modified to run through all the cities in Italy

--- scripts/immobiliare_main.py
+++ scripts/immobiliare_main.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
+import pandas as pd
+
 from real_estate_scraper.logging_mgmt import create_logger
 from real_estate_scraper.countries.italy.immobiliare import \
     get_immobiliare_scraper
+from real_estate_scraper.parsing import normalize_city_names
+import tqdm
 
 module_path = Path(__file__)
 module_name = module_path.stem
@@ -11,8 +15,16 @@ scraper = get_immobiliare_scraper(logger=logger,
                                   max_active_requests=5,
                                   requests_per_sec=5)
 
-cities = ["milano", "roma", "messina", "palermo", "anzio", "torino", "venezia",
-          "firenze", "alghero", "bari", "aosta", "trento", "ancona"]
+cities_file_path = Path.cwd().parent / "real_estate_scraper" / "countries" / "italy" / \
+                   "Elenco-comuni-italiani.xls"
 
-for city in cities:
+cities_df = pd.read_excel(cities_file_path)
+all_city_names = cities_df['Denominazione in italiano'].apply(
+    normalize_city_names).to_list()
+
+# cities = ["milano", "roma", "messina", "palermo", "anzio", "torino", "venezia",
+#           "firenze", "alghero", "bari", "aosta", "trento", "ancona"]
+
+for city in tqdm(all_city_names):
     scraper.download_to_db(city=city, deep=True)
+
